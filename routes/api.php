24da499f6d74ed195704d60dda66107ff75fe4d5
Kemal Karagöz <52218326+kkrgzz@users.noreply.github.com>
2025-02-07 18:52:18 +0300

    JWT Auth Operations Created

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+
+    Route::middleware(['middleware' => 'api', 'auth:api'])->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+        Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+        Route::post('me', [AuthController::class, 'me'])->name('me');
+    });
+});
